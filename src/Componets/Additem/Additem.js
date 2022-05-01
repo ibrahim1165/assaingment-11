3a64b665a5a27548b065ac2cd4b7bfcff365ebe2
Md Ibrahim <mdrafi15429@gmail.com>
2022-05-01 22:00:56 +0600

    post api and single post create done

--- src/Componets/Additem/Additem.js
+++ src/Componets/Additem/Additem.js
@@ -1,35 +1,162 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 const Additem = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => {
+        console.log(data);
+        const url = `http://localhost:5000/product`
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data),
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result);
+            })
+    }
     return (
-        <div>
-
-            <form>
-                <div className="bg-indigo-50 min-h-screen md:px-20 pt-6">
-                    <div className=" bg-white rounded-md px-6 py-10 max-w-2xl mx-auto">
-                        <h1 className="text-center text-2xl font-bold text-gray-500 mb-10">ADD POST</h1>
-                        <div className="space-y-4">
-                            <div>
-                                <label for="title" className="text-lx font-serif">Title:</label>
-                                <input type="text" placeholder="name" id="title" className="ml-2 outline-none py-1 px-2 text-md border-2 rounded-md"/>
-                            </div>
-                            <div>
-                                <label for="description" className="block mb-2 text-lg font-serif">Description:</label>
-                                <textarea id="description" cols="30" rows="10" placeholder="whrite here.." className="w-full font-serif  p-4 text-gray-600 bg-indigo-50 outline-none rounded-md"></textarea>
-                            </div>
-                            <div>
-                                <label for="name" className="text-lx font-serif">Name:</label>
-                                <input type="text" placeholder="name" id="name" className="ml-2 outline-none py-1 px-2 text-md border-2 rounded-md" />
-                            </div>
-                            <div>
-                                <label for="email" className="text-lx font-serif">Email:</label>
-                                <input type="text" placeholder="name" id="email" className="ml-2 outline-none py-1 px-2 text-md border-2 rounded-md" />
-                            </div>
-                            <button className=" px-6 py-2 mx-auto block rounded-md text-lg font-semibold text-indigo-100 bg-indigo-600  ">ADD POST</button>
-                        </div>
+        <div className="">
+            <h2 className="text-center m-4 d-block text-2xl text-bold ">Please add a service</h2>
+            <div class="block p-6 rounded-lg shadow-lg bg-white max-w-md mx-auto m-12">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div class="form-group mb-6">
+                        <input type="text" class="form-control block
+        w-full
+        px-3
+        py-1.5
+        text-base
+        font-normal
+        text-gray-700
+        bg-white bg-clip-padding
+        border border-solid border-gray-300
+        rounded
+        transition
+        ease-in-out
+        m-0
+        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput7"
+                            placeholder="Name"  {...register("name", { required: true, maxLength: 20 })} />
                     </div>
-                </div>
-            </form>
+                    <div class="form-group mb-6">
+                        <input type="number" class="form-control block
+        w-full
+        px-3
+        py-1.5
+        text-base
+        font-normal
+        text-gray-700
+        bg-white bg-clip-padding
+        border border-solid border-gray-300
+        rounded
+        transition
+        ease-in-out
+        m-0
+        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"  placeholder="price" {...register('price')} />
+                    </div>
+                    <div className="form-group mb-6">
+                        <input type="text" class="form-control block
+        w-full
+        px-3
+        py-1.5
+        text-base
+        font-normal
+        text-gray-700
+        bg-white bg-clip-padding
+        border border-solid border-gray-300
+        rounded
+        transition
+        ease-in-out
+        m-0
+        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Photo URL" {...register("img")} />
+                    </div>
+                    <div className="form-group mb-6">
+                        <input type="number" class="form-control block
+        w-full
+        px-3
+        py-1.5
+        text-base
+        font-normal
+        text-gray-700
+        bg-white bg-clip-padding
+        border border-solid border-gray-300
+        rounded
+        transition
+        ease-in-out
+        m-0
+        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="quenty" {...register("quenty")} />
+                    </div>
+                    <div className="form-group mb-6">
+                        <input type="Name" class="form-control block
+        w-full
+        px-3
+        py-1.5
+        text-base
+        font-normal
+        text-gray-700
+        bg-white bg-clip-padding
+        border border-solid border-gray-300
+        rounded
+        transition
+        ease-in-out
+        m-0
+        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="supplire" {...register("supplire")} />
+                    </div>
+
+                    <div class="form-group mb-6">
+                        <textarea
+                            class="
+                                form-control
+                                block
+                                w-full
+                                px-3
+                                py-1.5
+                                text-base
+                                font-normal
+                                text-gray-700
+                                bg-white bg-clip-padding
+                                border border-solid border-gray-300
+                                rounded
+                                transition
+                                ease-in-out
+                                m-0
+                            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                            id="text"
+                            rows="3"
+                            placeholder="Discreption" {...register('Discreption')}
+                        ></textarea>
+                    </div>
+                    <div class="form-group form-check text-center mb-6">
+                    </div>
+                   <Link to="/inventory">
+                   <button type="submit" class="
+                                     w-full
+                                    px-6
+                                    py-2.5
+                                    bg-blue-600
+                                    text-white
+                                    font-medium
+                                    text-xs
+                                    leading-tight
+                                    uppercase
+                                    rounded
+                                    shadow-md
+                                    hover:bg-blue-700 hover:shadow-lg
+                                    focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+                                    active:bg-blue-800 active:shadow-lg
+                                    transition
+                                    duration-150
+                                    ease-in-out">
+                                        Add
+                                    </button>
+                   </Link>
+                </form>
+            </div>
+
+
         </div>
     );
 };
